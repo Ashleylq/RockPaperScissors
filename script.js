5ecd705b63ce073642f03d58d15a296d649e7b23
Ashleylq <ironmanashley@gmail.com>
2025-02-27 22:30:15 +0530

    Added div messages

--- script.js
+++ script.js
@@ -13,43 +13,50 @@ function Computerchoice(){
 let playerChoice = ""
 let playerScore = 0;
 let computerScore = 0;
+const choiceMessage = document.createElement("div")
+const resultMessage = document.createElement("div")
+const scoreMessage = document.createElement("div")
 function Playround(){
     let computerChoice = Computerchoice();
-    console.log(`Your choice: ${playerChoice}
-        Computer's choice: ${computerChoice}`);
+    conso
+    choiceMessage.textContent = `Your choice: ${playerChoice}
+        Computer's choice: ${computerChoice}`;
     if ((playerChoice === "rock" && computerChoice === "scissors")||
 (playerChoice === "paper" && computerChoice === "rock")||
 (playerChoice === "scissors" && computerChoice === "paper")){
-    console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+    resultMessage.textContent = `You win! ${playerChoice} beats ${computerChoice}`;
     playerScore += 1;
 }
     else if ((computerChoice === "rock" && playerChoice === "scissors")||
 (computerChoice === "paper" && playerChoice === "rock")||
 (computerChoice === "scissors" && playerChoice === "paper")){
-    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+    resultMessage.textContent = `You lose! ${computerChoice} beats ${playerChoice}`;
     computerScore += 1;
 }
 else{
-    console.log("It's a tie!");
+    resultMessage.textContent = "It's a tie!";
 }
-console.log(`Your score: ${playerScore}
-    Computer's score: ${computerScore}`);
+scoreMessage.textContent = `Your score: ${playerScore}
+    Computer's score: ${computerScore}`;
 }
 const container = document.querySelector(".container");
 const rockButton = document.createElement("button");
 rockButton.textContent = "rock"
 rockButton.addEventListener("click", () =>  playerChoice = "rock")
-container.appendChild("rockButton");
+container.appendChild(rockButton);
 const paperButton = document.createElement("button");
 paperButton.textContent = "paper"
 paperButton.addEventListener("click", () => playerChoice = "paper")
-container.appendChild("paperButton")
+container.appendChild(paperButton)
 const scissorsButton = document.createElement("button");
 scissorsButton.textContent = "scissors"
 scissorsButton.addEventListener("click", () => playerChoice = "scissors")
-container.appendChild("scissorsButton")
+container.appendChild(scissorsButton)
 document.addEventListener("click", function(e){
     if (e.target && e.target.nodeName == "button"){
         Playround()
     }
 })
+container.appendChild(choiceMessage);
+container.appendChild(resultMessage);
+container.appendChild(scoreMessage);
