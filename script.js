31f9e6011d628f059361c3ce07b8037cf760c303
Ashleylq <ironmanashley@gmail.com>
2025-03-09 15:13:01 +0530

    Add Playgame and Playround functions

--- script.js
+++ script.js
@@ -22,3 +22,37 @@ function Playerchoice(){
         Playerchoice();
     }
 }
+
+let playerScore = 0;
+ let computerScore = 0;
+ function Playround(){
+     let computerChoice = Computerchoice();
+     let playerChoice = Playerchoice();
+     console.log(`Your choice: ${playerChoice}
+         Computer's choice: ${computerChoice}`);
+     if ((playerChoice === "rock" && computerChoice === "scissors")||
+ (playerChoice === "paper" && computerChoice === "rock")||
+ (playerChoice === "scissors" && computerChoice === "paper")){
+     console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+     playerScore += 1;
+ }
+     else if ((computerChoice === "rock" && playerChoice === "scissors")||
+ (computerChoice === "paper" && playerChoice === "rock")||
+ (computerChoice === "scissors" && playerChoice === "paper")){
+     console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+     computerScore += 1;
+ }
+ else{
+     console.log("It's a tie!");
+ }
+ console.log(`Your score: ${playerScore}
+     Computer's score: ${computerScore}`);
+ }
+ 
+ function Playgame(){
+     playerScore = 0;
+     computerScore = 0;
+     for (let i = 0; i < 5; i++){
+         Playround();
+     }
+ }
