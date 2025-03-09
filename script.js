3d5a671c22154c320d9a9310b652cd3e5ed77be0
Ashleylq <ironmanashley@gmail.com>
2025-03-09 15:11:05 +0530

    Add Computerchoice and Playerchoice functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function Computerchoice(){
+    let randomChoice = Math.floor(Math.random()*3);
+    if (randomChoice === 1){
+        return "rock";
+    }
+    else if (randomChoice === 2){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
 
+function Playerchoice(){
+    let randomChoice = prompt("What do you choose?");
+    let choice = randomChoice.toLowerCase();
+    if (choice === "rock" || choice === "paper" || choice === "scissors"){
+        return choice;
+    }
+    else{
+        alert("Invalid input");
+        Playerchoice();
+    }
+}
